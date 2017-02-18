9b66a858857c939baaf58b51a5ff187a56edad1b
wacgoo <tatwaiho@gmail.com>
2017-02-18 00:13:25 -0800

    create http server
    Example for creating an http server

---
+++ js/httpServerExample.js
@@ -0,0 +1,10 @@
+
+var http = require('http');
+
+var server = http.createServer( function(request, response) {
+    console.log('got a request!');
+    response.write('hi');
+    response.end();
+});
+
+server.listen(3000);
