05faba828c727527ab64b88366ba3a3fba03cf55
wacgoo <tatwaiho@gmail.com>
2017-02-18 17:15:20 -0800

    headless function
    change to headless function

--- js/httpServerExample.js
+++ js/httpServerExample.js
@@ -1,10 +1,12 @@
 
 var http = require('http');
 
-var server = http.createServer( function(request, response) {
+var server = http.createServer((request, response) => {
+// var server = http.createServer( function(request, response) {
     console.log('got a request!');
     response.write('hi');
     response.end();
 });
 
 server.listen(3000);
+
